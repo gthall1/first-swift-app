b363a1868907eb96e384f09bfe3b4b75aedaf490
Griff Hall <griffhall1@gmail.com>
2014-11-19 10:38:33 -0600

    Initial Commit

---
+++ Example App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example App
+//
+//  Created by Griff Hall on 11/19/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
